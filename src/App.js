ac140282eeb1d989fb2d3cef7b8a3fb8e73c8561
BIXMAC <BFRIMPONG>
2021-05-19 10:52:48 +0000

    combined both the sign up and login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Signup from './component/login/signup';
-import Login from './component/login/login';
-// import SignInOutContainer from "./component/login/index"
+import SignInOutContainer from "./component/login/index"
 
 const App = () =>{
   return(
     <div>
-      <Signup />
-      <Login /> 
-      {/* <SignInOutContainer /> */}
+
+      <SignInOutContainer />
 
     </div>
 
