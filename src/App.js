3af83955c695c58d69ffd6b1545243b0db3687f6
BIXMAC <BFRIMPONG>
2021-05-19 10:41:23 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Signup from './component/login/signup';
+// import Login from './component/login/login';
+// import SignInOutContainer from "./component/login/index"
+
+const App = () =>{
+  return(
+    <div>
+      <Signup />
+      {/* <Login />  */}
+      {/* <SignInOutContainer /> */}
+
+    </div>
+
+  )
+}
+export default App;
